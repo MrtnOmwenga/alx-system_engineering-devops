c807eb7fd479ab54e72d30ae7e34a43a5257a18b
MrtnOmwenga <martin36449@gmail.com>
2023-03-24 12:04:54 -0700

    Create file

---
+++ 0x0A-configuration_management/manifests/0-create_a_file.pp
@@ -0,0 +1,15 @@
+# Creates file school in /tmp
+node 'default' {
+  # Create file
+  file{ '/tmp/school':
+    ensure  =>'present',
+    owner   =>'www-data',
+    group   =>'www-data',
+    mode    =>'0744',
+    content =>'I love Puppet'
+  }
+}
+
+# node 'stapp01.stratos.xfusioncorp.com' {
+#  include file_creator
+# }
