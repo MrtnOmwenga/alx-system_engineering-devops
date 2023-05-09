a2875e90813a08cb17a6d1b599057a101ccf127b
MrtnOmwenga <martin36449@gmail.com>
2023-05-09 08:45:25 -0700

    Pycodestyle

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,7 @@ import requests
 def number_of_subscribers(subreddit):
     """ Get number of subscribers """
 
-    url = "https://www.reddit.com/r/" + subreddit + "/about.json"
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     data = requests.get(url, headers={'User-Agent':
                                       '0x16-api_advanced:project:\
 v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()
