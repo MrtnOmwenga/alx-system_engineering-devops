30f217051f5663aa57bc6a118e353f8a96e01560
MrtnOmwenga <martin36449@gmail.com>
2023-05-09 08:36:18 -0700

    Subs.py

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Get number of Subscribers of a subreddit """
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """ Get number of subscribers """
+
+    data = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
+                     headers={'User-Agent': '0x16-api_advanced:project:\
+v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()
+    subs = data.get("data", {}).get("subscribers", 0)
+    return (subs)
